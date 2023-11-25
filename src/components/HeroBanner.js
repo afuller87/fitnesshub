d205ae901c93f13424a66af4fd57819e0e7370b9
afuller87 <amyfuller19@gmail.com>
2023-11-25 00:00:43 -0500

    Added New Components and Updated Home

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Box, Stack, Typography, Button } from '@mui/material';
+
+const HeroBanner = () => {
+    return (
+        <Box sx={{
+            marginTop
+        }}>
+            <Typography color="goldenrod" 
+            fontWeight="600px" 
+            fontSize="26px">
+                Fitness HUB
+            </Typography>
+        </Box>
+    )
+}
+
+export default HeroBanner
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,38 @@ import React from "react";
 import { Link } from 'react-router-dom';
 import { Stack } from '@mui/material'
 
-import { Logo } from '../media/images/Logo.png';
+import Logo from '../media/images/Logo.png';
 
 const Navbar = () => {
     return (
-        <Stack>
-            <Link>
-                <img src={Logo} />
+        <Stack direction="row"
+        justifyContent="space-around" 
+            sx={{ gap: {
+            sm: '122px', xs: '40px'},
+            marginTop: { sm: '32px', xs: '20px'},
+            justfyContent: 'none'}}
+            px="20px">
+            <Link to="/">
+                <img src={Logo} alt="logo" style={{ 
+                    width: '150px',
+                    height: '150px',
+                    margin: '0 20px'
+                }} />
             </Link>
+            <Stack
+                direction="row"
+                gap="50px"
+                fontSize="24px"
+                alignItems="center"
+            >
+                <Link to="/" style={{
+                    textDecoration: 'none',
+                    color: "teal",
+                    borderBottom: '3px solid goldenrod'}}>Home</Link>
+                <a href="#exercises" style={{
+                    textDecoration: 'none',
+                    color: 'teal'}}>Exercises</a>
+            </Stack>
         </Stack>
     )
 }
